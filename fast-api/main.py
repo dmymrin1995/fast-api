1489311c63d2210431cf9b0b5258dfc0d5cff8b9
dmymrin1995 <81430697+dmymrin1995@users.noreply.github.com>
2022-12-27 15:22:39 +0800

    Add files via upload

---
+++ fast-api/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from db.base import database
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {"message": 'Hello world!'}
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port = 8000, host = "0.0.0.0", reload = True)
